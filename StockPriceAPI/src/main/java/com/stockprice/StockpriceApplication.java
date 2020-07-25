35319d67f17d85ddaa33c5da4ceddd9c9e2af6ee
Elvis Leung <leung251@gmail.com>
2020-07-25 21:52:32 +0800

    Moved API to a sub-directory and added Eureka server application

--- StockPriceAPI/src/main/java/com/stockprice/StockpriceApplication.java
+++ StockPriceAPI/src/main/java/com/stockprice/StockpriceApplication.java
@@ -2,8 +2,10 @@ package com.stockprice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StockpriceApplication {
 
     public static void main(String[] args) {
